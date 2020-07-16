fdb035b55850727debf6da3c03c6ccbff83672d3
ChrisAraneo <chris.araneo@gmail.com>
2020-07-16 22:45:14 +0200

    Captcha 16 VII

--- src/main/java/com/chrisaraneo/mwl/MusicWebLibraryApplication.java
+++ src/main/java/com/chrisaraneo/mwl/MusicWebLibraryApplication.java
@@ -15,13 +15,4 @@ public class MusicWebLibraryApplication {
 		SpringApplication.run(MusicWebLibraryApplication.class, args);
 	}
 	
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("api/**").allowedOrigins("*");
-//			}
-//		};
-//	}
 }
--- src/main/java/com/chrisaraneo/mwl/controller/IndexController.java
+++ src/main/java/com/chrisaraneo/mwl/controller/IndexController.java
@@ -1,8 +1,13 @@
 package com.chrisaraneo.mwl.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.chrisaraneo.mwl.model.extended.EmptyJson;
+import com.chrisaraneo.mwl.security.RequiresCaptcha;
 
 @RestController
 @RequestMapping("/")
@@ -11,6 +16,12 @@ public class IndexController {
     @GetMapping
     public String getIndex() {
         return "Index";
+    }
+    
+    @PostMapping
+    @RequiresCaptcha
+    public Object postIndex(){
+        return new EmptyJson();
     }
     
 }
